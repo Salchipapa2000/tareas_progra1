8c07995f4843a0924ee1ab38a3aaab18c5a6c6ad
Jeanneth Moran <magicentersocialmedia@gmail.com>
2025-09-06 09:19:01 -0600

    Tercera versión de la calculadora con funciones sumar, restar, multiplicar y dividir

--- calculadora.rb
+++ calculadora.rb
@@ -1,12 +1,21 @@
 def sumar(a, b)
-return a + b
+  return a + b
 end
+
 def restar(a, b)
-return a - b
+  return a - b
 end
+
 def multiplicar (a, b)
-return a * b
+  return a * b
 end
-puts "Suma: #{sumar(10, 5)}"
-puts "Resta: #{restar(10, 5)}"
-puts "Multiplicar: #{multiplicar(10, 5)}"
+
+def dividir (a, b)
+  return a/b
+end
+  
+  puts "Suma: #{sumar(10, 5)}"
+  puts "Resta: #{restar(10, 5)}"
+  puts "Multiplicar: #{multiplicar(10, 5)}"
+  puts "Dividir: #{dividir(10, 5)}"
+
