12e4084ee6e5572c2b1f9542919135f292e05478
Jeanneth Moran <magicentersocialmedia@gmail.com>
2025-09-06 09:03:51 -0600

    Segunda versión de la calculadora con funciones sumar y restar

--- calculadora.rb
+++ calculadora.rb
@@ -4,5 +4,9 @@ end
 def restar(a, b)
 return a - b
 end
+def multiplicar (a, b)
+return a * b
+end
 puts "Suma: #{sumar(10, 5)}"
 puts "Resta: #{restar(10, 5)}"
+puts "Multiplicar: #{multiplicar(10, 5)}"
