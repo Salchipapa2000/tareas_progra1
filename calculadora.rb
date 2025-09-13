472e248445c244ef5601fa24fe5906e0b734582a
Jeanneth Moran <magicentersocialmedia@gmail.com>
2025-09-12 21:59:44 -0600

    Versión 4 de la calculadora con funciones, uso de 3 números

--- calculadora.rb
+++ calculadora.rb
@@ -1,26 +1,28 @@
-def sumar(a, b)
-  return a + b
+def sumar(a, b, c)
+  return a + b + c
 end
 
-def restar(a, b)
-  return a - b
+def restar(a, b, c)
+  return a - b - c
 end
 
-def multiplicar (a, b)
-  return a * b
+def multiplicar (a, b, c)
+  return a * b * c
 end
 
-def dividir (a, b)
-  return a/b
+def dividir (a, b, c)
+  return a/b/c
 end
 
 puts "Ingrese el primer número:"
 x = gets.to_i
 puts "Ingrese el segundo número:"
 y = gets.to_i
+puts "Ingrese el tercer número:"
+c = gets.to_i
 
-  puts "Suma: #{sumar(x, y)}"
-  puts "Resta: #{restar(x, y)}"
-  puts "Multiplicar: #{multiplicar(x, y)}"
-  puts "Dividir: #{dividir(x, y)}"
+  puts "Suma: #{sumar(x, y, c)}"
+  puts "Resta: #{restar(x, y, c)}"
+  puts "Multiplicar: #{multiplicar(x, y, c)}"
+  puts "Dividir: #{dividir(x, y, c)}"
 
